5697f11568d132dbaa967a79f34d981a80c8a0f0
Rahat <rahat@rahatcodes.com>
2023-05-22 09:30:54 +0400

    updat pfills

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,36 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill'
+// import { NodeModulesPolyfillPlugin } from '@esbuild-plugins/node-modules-polyfill';
 
 export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
     esbuildOptions: {
+      target: "es2020",
       define: {
         global: "globalThis",
       },
       plugins: [
         NodeGlobalsPolyfillPlugin({
-            buffer: true
+            buffer: true,
+            process: true,
         })
     ]
     },
   },
+  define: {
+    'process.env': process.env,
+  },
   resolve: {
     alias: {
-      process: "process/browser",
+      assert: "assert",
+      buffer: "buffer",
+      crypto: "crypto-browserify",
+      http: "stream-http",
+      https: "https-browserify",
+      os: "os-browserify/browser",
+      // process: "process/browser",
       stream: "stream-browserify",
       util: "util",
     },
